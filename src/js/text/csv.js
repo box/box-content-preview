0e956771bd3e39efb3005f4b305c01e3492dd5d9
Priyajeet Hora <priyajeet@gmail.com>
2015-11-06 15:47:04 -0800

    Fixing csv stuff

--- src/js/text/csv.js
+++ src/js/text/csv.js
@@ -10,6 +10,7 @@ import { Table, Column } from 'fixed-data-table';
 
 let Promise = global.Promise;
 let document = global.document;
+let URL = global.URL;
 let Box = global.Box || {};
 let Papa = global.Papa;
 
@@ -36,21 +37,45 @@ class CSV extends TextBase {
      */
     load(csvUrl) {
         return new Promise((resolve, reject) => {
-            Papa.SCRIPT_PATH = this.options.scripts[0];
-            Papa.parse(csvUrl, {
-                worker: true,
-                download: true,
-                error: (err, file, inputElem, reason) => {
-                    reject(reason);
-                },
-                complete: (results) => {
-                    resolve(this);
-                    this.loaded = true;
-                    this.emit('load');
-                    this.renderCSV(results.data);
-                }
-            });
+            let papaWorkerUrl = this.options.asset.replace('{{asset_name}}', 'papaparse.js');
+
+            fetch(papaWorkerUrl)
+                .then((response) => response.blob())
+                .then((papaWorkerBlob) => {
+                    Papa.SCRIPT_PATH = URL.createObjectURL(papaWorkerBlob);
+                    Papa.parse(csvUrl, {
+                        worker: true,
+                        download: true,
+                        error: (err, file, inputElem, reason) => {
+                            reject(reason);
+                        },
+                        complete: (results) => {
+                            this.finishLoading(results.data, resolve);
+                        }
+                    });
+                });
         });
+    }
+
+    /**
+     * Finishes loading the csv data
+     *
+     * @param {String} data The data content to load
+     * @param {Function} resolve Resolution handler
+     * @private
+     * @returns {void}
+     */
+    finishLoading(data, resolve) {
+        this.renderCSV(data);
+        URL.revokeObjectURL(papaWorkerBlob);
+
+        if (this.options.ui !== false) {
+            this.loadUI();
+        }
+
+        resolve(this);
+        this.loaded = true;
+        this.emit('load');
     }
 
     /**
@@ -61,7 +86,7 @@ class CSV extends TextBase {
      */
     renderCSV(data) {
         ReactDOM.render(
-            <Table rowHeight={50} rowGetter={(index) => data[index]} rowsCount={data.length} width={this.csvEl.clientWidth - 100} maxHeight={this.csvEl.clientHeight - 100} headerHeight={10}>
+            <Table rowHeight={50} rowGetter={(index) => data[index]} rowsCount={data.length} width={this.csvEl.clientWidth} maxHeight={this.csvEl.clientHeight} headerHeight={10}>
                 {data[0].map((object, index) => {
                     return <Column width={150} allowCellsRecycling={true} dataKey={index} />;
                 })}
