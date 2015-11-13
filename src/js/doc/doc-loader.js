d437b80f52c450abb1d5828cd9a8ab62151f4b69
Priyajeet Hora <priyajeet@gmail.com>
2015-11-12 16:05:35 -0800

    Adding an init function to loaders that can be overriden to do some init stuff if needed

--- src/js/doc/doc-loader.js
+++ src/js/doc/doc-loader.js
@@ -2,6 +2,8 @@
 
 import AssetLoader from '../assets';
 
+const SCRIPTS = [ 'compatibility.js', 'pdf.js', 'pdf_viewer.js', 'pdf.worker.js', 'document.js' ];
+
 // Order of the viewers matters. Prefer original before others. Go from specific to general.
 // For example, a pdf file can be previewed both natively (majority use case) using the original
 // representation but can fallback to using the pdf representation (for watermarked versions).
@@ -9,21 +11,21 @@ const VIEWERS = [
     {
         REPRESENTATION: 'original',
         EXTENSIONS: [ 'pdf' ],
-        SCRIPTS: [ 'compatibility.js', 'pdf.js', 'pdf_viewer.js', 'document.js' ],
+        SCRIPTS: SCRIPTS,
         STYLESHEETS: [ 'pdf_viewer.css', 'document.css' ],
         CONSTRUCTOR: 'Document'
     },
     {
         REPRESENTATION: 'pdf',
         EXTENSIONS: [ 'doc', 'docx', 'gdoc', 'gsheet', 'msg', 'odp', 'odt', 'ods', 'pdf', 'rtf', 'wpd', 'xhtml', 'xls', 'xlsm', 'xlsx', 'xml', 'xsd', 'xsl' ],
-        SCRIPTS: [ 'compatibility.js', 'pdf.js', 'pdf_viewer.js', 'document.js' ],
+        SCRIPTS: SCRIPTS,
         STYLESHEETS: [ 'pdf_viewer.css', 'document.css' ],
         CONSTRUCTOR: 'Document'
     },
     {
         REPRESENTATION: 'pdf',
         EXTENSIONS: [ 'ppt', 'pptx' ],
-        SCRIPTS: [ 'compatibility.js', 'pdf.js', 'pdf_viewer.js', 'presentation.js' ],
+        SCRIPTS: SCRIPTS,
         STYLESHEETS: [ 'pdf_viewer.css', 'presentation.css' ],
         CONSTRUCTOR: 'Presentation'
     },
@@ -38,6 +40,19 @@ class DocLoader extends AssetLoader {
     constructor() {
         super();
         this.viewers = VIEWERS;
+    }
+
+    /**
+     * Some initialization stuff
+     *
+     * @override
+     * @param {Object} options
+     * @returns {void}
+     */
+    init(options) {
+        // Since the pdf worker is pretty big, lets prefetch it
+        let pdfWorkerUrl = this.createAssetUrl(options.location.hrefTemplate)('pdf.worker.js');
+        this.prefetchAssets([ pdfWorkerUrl ]);
     }
 }
 
