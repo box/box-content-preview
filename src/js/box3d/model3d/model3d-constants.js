8803697ae23add3082897647542e9b7c02936f5e
Michael Bond <mbond@box.com>
2016-02-09 16:27:50 -0800

    Adding in code for supporting change of camera perspective. UI and metadata junk still needs to be added

--- src/js/box3d/model3d/model3d-constants.js
+++ src/js/box3d/model3d/model3d-constants.js
@@ -5,3 +5,6 @@ export const EVENT_MISSING_ASSET = 'missingAsset';
 export const EVENT_ROTATE_ON_AXIS = 'rotateOnAxis';
 export const EVENT_SAVE_SCENE_DEFAULTS = 'sceneSave';
 export const EVENT_SET_RENDER_MODE = 'setRenderMode';
+export const EVENT_SET_CAMERA_PROJECTION = 'setCameraProjection';
+export const CAMERA_PROJECTION_PERSPECTIVE = 'Perspective';
+export const CAMERA_PROJECTION_ORTHOGRAPHIC = 'Orthographic';
