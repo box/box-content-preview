5646c4ec891e3a7ae07980a8b03501b188ab5803
Priyajeet Hora <priyajeet@gmail.com>
2016-05-21 10:02:53 -0700

    Switching to prefer local repo over remote repo

--- build/jenkins_ci.sh
+++ build/jenkins_ci.sh
@@ -5,18 +5,18 @@ export NODE_PATH=$NODE_PATH:./node_modules
 # Clean node modules, re-install dependencies, and build assets
 build_assets() {
 
-    echo "-------------------------------------------------------------"
-    echo "Installing node modules from https://registry.nodejitsu.com"
-    echo "-------------------------------------------------------------"
-    if npm install --registry https://registry.nodejitsu.com; then
+    echo "---------------------------------------------------------------------------------------------------------------------------"
+    echo "Installing node modules from http://maven-vip.dev.box.net:8150/nexus/content/groups/npm-all/ OR https://registry.npmjs.org"
+    echo "---------------------------------------------------------------------------------------------------------------------------"
+    if npm install; then
         echo "----------------------------------------------------"
         echo "Installed node modules."
         echo "----------------------------------------------------"
     else
-        echo "---------------------------------------------------------------------------------------------------------------------------"
-        echo "Installing node modules from http://maven-vip.dev.box.net:8150/nexus/content/groups/npm-all/ OR https://registry.npmjs.org"
-        echo "---------------------------------------------------------------------------------------------------------------------------"
-        if npm install; then
+        echo "-------------------------------------------------------------"
+        echo "Installing node modules from https://registry.nodejitsu.com"
+        echo "-------------------------------------------------------------"
+        if npm install --registry https://registry.nodejitsu.com; then
             echo "----------------------------------------------------"
             echo "Installed node modules."
             echo "----------------------------------------------------"
