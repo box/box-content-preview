83229b32066a2668f3a7e1ff390f7bc630126f7f
Tony Jin <tjin@box.com>
2016-05-26 13:18:35 -0700

    Minor tweaks
    - Increasing timeout to 20s
    - Updating image test

--- src/lib/base.js
+++ src/lib/base.js
@@ -14,7 +14,7 @@ import {
     SELECTOR_BOX_PREVIEW
 } from './constants';
 
-const LOAD_TIMEOUT_MS = 15000; // 15s
+const LOAD_TIMEOUT_MS = 20000; // 20s
 const RESIZE_WAIT_TIME_IN_MILLIS = 300;
 const OPTIONS = {
     ui: true
