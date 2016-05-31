6be2f55212dfadba6de145512d9fe6019fde00dc
Tony Jin <tjin@box.com>
2016-05-31 15:59:07 -0700

    Fixing unit tests

--- src/lib/__tests__/logger-test.js
+++ src/lib/__tests__/logger-test.js
@@ -5,18 +5,18 @@ describe('Logger', () => {
         const logger = new Logger('FOO');
         const log = logger.done();
 
-        assert.equal(0, log.time.total);
-        assert.equal(0, log.time.conversion);
-        assert.equal(0, log.time.rendering);
+        assert.ok(log.time.total < 5, 'Total time should be correct');
+        assert.ok(log.time.conversion < 5, 'Conversion time should be correct');
+        assert.ok(log.time.rendering < 5, 'Rendering time should be correct');
 
-        assert.equal(undefined, log.type);
-        assert.equal('FOO', log.locale);
-        assert.equal('preview', log.event);
+        assert.equal(undefined, log.type, 'Type should be correct');
+        assert.equal('FOO', log.locale, 'Locale should be correct');
+        assert.equal('preview', log.event, 'Event should be correct');
 
-        assert.equal(undefined, log.file);
+        assert.equal(undefined, log.file, 'File should be correct');
 
-        assert.notOk(log.cache.hit);
-        assert.notOk(log.cache.stale);
+        assert.notOk(log.cache.hit, 'Cache should not be hit');
+        assert.notOk(log.cache.stale, 'Cache should not be stale');
     });
 
     it('should set and get correctly', () => {
@@ -35,17 +35,17 @@ describe('Logger', () => {
 
         const log = logger.done();
 
-        assert.equal('number', typeof log.time.total);
-        assert.equal('number', typeof log.time.conversion);
-        assert.equal('number', typeof log.time.rendering);
-        assert.equal(log.time.total, log.time.conversion + log.time.rendering);
+        assert.equal('number', typeof log.time.total, 'Total time should be a number');
+        assert.equal('number', typeof log.time.conversion, 'Conversion time should be a number');
+        assert.equal('number', typeof log.time.rendering, 'Conversion time should be a number');
+        assert.equal(log.time.total, log.time.conversion + log.time.rendering, 'Total time should add up');
 
-        assert.equal('BAR', log.type);
-        assert.equal('FOO', log.locale);
+        assert.equal('BAR', log.type, 'Type should be correct');
+        assert.equal('FOO', log.locale, 'Locale should be correct');
 
-        assert.equal(1, log.file.id);
+        assert.equal(1, log.file.id, 'File ID should be correct');
 
-        assert.ok(log.cache.hit);
-        assert.ok(log.cache.stale);
+        assert.ok(log.cache.hit, 'Cache should be hit');
+        assert.ok(log.cache.stale, 'Cache should be stale');
     });
 });
