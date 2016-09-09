c274427cdc775b183f118573dff27dff84044220
Tony Jin <tjin@box.com>
2016-09-09 15:47:16 -0700

    Stop propagation of click events on notification

--- src/lib/__tests__/notification-test.js
+++ src/lib/__tests__/notification-test.js
@@ -1,5 +1,8 @@
+/* eslint-disable no-unused-expressions */
 import Notification from '../notification';
 let notif;
+
+const sandbox = sinon.sandbox.create();
 
 describe('Notification', () => {
     before(() => {
@@ -13,31 +16,37 @@ describe('Notification', () => {
 
     afterEach(() => {
         fixture.cleanup();
+        sandbox.verifyAndRestore();
     });
 
     describe('Notification()', () => {
         it('should have the right classes assigned', () => {
             assert.equal(notif.notificationEl.className, 'notification box-preview-is-hidden');
         });
+
         it('shouldn\'t have any message or button text', () => {
             assert.equal(notif.buttonEl.textContext, undefined);
             assert.equal(notif.messageEl.textContext, undefined);
         });
+
         it('should have the correct parent wrapper', () => {
             assert.notEqual(notif.notificationEl.parentNode, 'undefined');
             assert.equal(notif.notificationEl.parentNode.className, 'notifications-wrapper');
         });
     });
+
     describe('show()', () => {
         it('should properly show the notification', () => {
             notif.show('test', 'test');
             assert.equal(notif.notificationEl.className, 'notification');
         });
+
         it('should add text to both the button and the message', () => {
             notif.show('test', 'test');
             assert.equal(notif.messageEl.textContent, 'test');
             assert.equal(notif.buttonEl.textContent, 'test');
         });
+
         it('should not show button text if the notification is hidden and re-shown with no text', () => {
             notif.show('test', 'test');
             notif.hide();
@@ -46,10 +55,36 @@ describe('Notification', () => {
             assert.equal(notif.buttonEl.textContent, __('notification_button_default_text'));
         });
     });
+
     describe('hide()', () => {
         it('should be properly hidden', () => {
             notif.hide();
             assert.equal(notif.notificationEl.className, 'notification box-preview-is-hidden');
+        });
+    });
+
+    describe('clickHandler()', () => {
+        it('should stop propagation of event', () => {
+            const event = {
+                stopPropagation: sandbox.stub()
+            };
+
+            notif.clickHandler(event);
+
+            expect(event.stopPropagation).to.have.been.called;
+        });
+
+        it('should hide notification if button is clicked', () => {
+            sandbox.stub(notif, 'hide');
+
+            const event = {
+                stopPropagation: sandbox.stub(),
+                target: notif.notificationEl.querySelector('button')
+            };
+
+            notif.clickHandler(event);
+
+            expect(notif.hide).to.have.been.called;
         });
     });
 });
