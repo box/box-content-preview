b0e15b17d16903cca849d0f99c80c7314e6bbd4d
bhh1988 <bhh1988@gmail.com>
2017-05-07 12:47:06 -0700

    Chore: Allow console logging to show in tests (#100)
    Using solution near bottom here:
    https://github.com/karma-runner/karma-mocha/issues/47

--- build/karma.conf.js
+++ build/karma.conf.js
@@ -40,6 +40,12 @@ module.exports = (config) => config.set({
 
     basePath: '..',
 
+    browserConsoleLogOptions: {
+        level: 'log',
+        format: '%b %T: %m',
+        terminal: true
+    },
+
     browsers: ['PhantomJS'],
 
     browserNoActivityTimeout: 100000,
