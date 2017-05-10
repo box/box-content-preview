fa7c74194f7c28ab48d41f42ffcdb636ba4d5c41
Bryan Huh <bryan@box.com>
2017-05-10 14:36:57 -0700

    Upgrade: Upgrade Shaka-player to 2.1.1
    Changes here: https://github.com/google/shaka-player/releases
    Notably, the upgrade adds support for asynchronous response filters,
    which can be used for handling when the token is expired. This commit
    also modifies DashViewer to use the new player API (getVariantTracks and
    getTextTracks), instead of the deprecated getTracks and selectTrack
    methods

--- build/karma.conf.js
+++ build/karma.conf.js
@@ -1,7 +1,7 @@
 const webpackConfig = require('./webpack.karma.config');
 
 const DOC_STATIC_ASSETS_VERSION = '0.121.1';
-const MEDIA_STATIC_ASSETS_VERSION = '0.120.1';
+const MEDIA_STATIC_ASSETS_VERSION = '0.122.0';
 const MODEL3D_STATIC_ASSETS_VERSION = '0.115.0';
 const SWF_STATIC_ASSETS_VERSION = '0.112.0';
 const TEXT_STATIC_ASSETS_VERSION = '0.114.0';
