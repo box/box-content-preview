2b2572b4fda310c6f83eb0d576dbf7f6cdb4f1e3
Justin Holdstock <jholdstock@box.com>
2018-01-17 11:01:45 -0600

    Chore: Add docs to viewer events

--- src/lib/events.js
+++ src/lib/events.js
@@ -1,13 +1,13 @@
 // Events emitted by Viewers
 // eslint-disable-next-line import/prefer-default-export
 export const VIEWER_EVENTS = {
-    download: 'download',
-    reload: 'reload',
-    load: 'load',
-    progressStart: 'progressstart',
-    progressEnd: 'progressend',
-    notificationShow: 'notificationshow',
-    notificationHide: 'notificationhide',
-    mediaEndAutoplay: 'mediaendautoplay',
-    generic: 'viewerevent'
+    download: 'download', // Begin downloading the file.
+    reload: 'reload', // Reload preview.
+    load: 'load', // Preview is finished loading.
+    progressStart: 'progressstart', // Begin using loading indicator.
+    progressEnd: 'progressend', // Stop using loading indicator.
+    notificationShow: 'notificationshow', // Show notification modal.
+    notificationHide: 'notificationhide', // Hide notification modal.
+    mediaEndAutoplay: 'mediaendautoplay', // Media playback has completed, with autoplay enabled.
+    generic: 'viewerevent' // The generic default viewer event
 };
