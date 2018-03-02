fa05342df599ac26ea7f25066f8735e4a0a6b65b
Jeremy Press <jpress@box.com>
2018-03-02 09:10:46 -0800

    Chore: Refactor and add a sanity error check

--- functional-tests/sanity_test.js
+++ functional-tests/sanity_test.js
@@ -1,11 +1,25 @@
 Feature('Sanity', { retries: 3 });
 const bpLoaded = '.bp-loaded';
+const FILE_ID_DOC = '93392244621';
 
 Before((I) => {
     I.amOnPage('/functional-tests/index.html');
 });
 
 Scenario('Sanity test @ci @chrome @firefox @edge @safari @ie @android @ios', (I) => {
+    Before(() => {
+        I.executeScript(() => {
+            window.showPreview(FILE_ID_DOC, {
+                showDownload: true
+            });
+        });
+    });
+    I.waitForElement(bpLoaded);
+    I.waitForVisible(bpLoaded);
+    I.waitForText('The Content Platform for Your Apps');
+});
+
+Scenario('Sanity error test @ci @chrome @firefox @edge @safari @ie @android @ios', (I) => {
     I.waitForElement(bpLoaded);
     I.waitForVisible(bpLoaded);
     I.waitForText('The Content Platform for Your Apps');
