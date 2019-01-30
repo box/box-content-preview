48c659c6206f1b2a409b39832fc368fa46064161
Conrad Chan <conradchan@box.com>
2019-01-29 16:59:22 -0800

    Chore: PR comments

--- test/support/commands.js
+++ test/support/commands.js
@@ -1,8 +1,8 @@
 Cypress.Commands.add('getByTestId', (testId) => cy.get(`[data-testid="${testId}"]`));
 Cypress.Commands.add('getByTitle', (title) => cy.get(`[title="${title}"]`));
 Cypress.Commands.add('showPreview', (token, fileId) => {
-    cy.get('[data-cy="token"]').type(token);
-    cy.get('[data-cy="token-set"]').click();
-    cy.get('[data-cy="fileid"]').type(fileId);
-    cy.get('[data-cy="fileid-set"]').click();
+    cy.get('[data-testid="token"]').type(token);
+    cy.get('[data-testid="token-set"]').click();
+    cy.get('[data-testid="fileid"]').type(fileId);
+    cy.get('[data-testid="fileid-set"]').click();
 });
