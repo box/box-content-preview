0093c9316a7d11e115b964163d779e87481f3b83
Conrad Chan <conrad@box.com>
2019-02-04 16:17:12 -0800

    Chore: Adding Cypress tests to cover existing functional tests (#912)

--- test/integration/sanity/Sanity.e2e.test.js
+++ test/integration/sanity/Sanity.e2e.test.js
@@ -1,19 +1,15 @@
 // <reference types="Cypress" />
 describe('Preview Sanity', () => {
-    let token;
+    const token = Cypress.env('ACCESS_TOKEN');
+    const fileId = Cypress.env('FILE_ID_DOC');
 
     beforeEach(() => {
-        token = Cypress.env('ACCESS_TOKEN');
         cy.visit('/');
     });
 
     it('Should load a document preview', () => {
-        const fileId = Cypress.env('FILE_ID_DOC');
-
         // Show the preview
         cy.showPreview(token, fileId);
-        // Wait for .bp to load viewer
-        cy.getByTestId('bp').should('have.class', 'bp-loaded')
         // Assert document content is present
         cy.contains('The Content Platform for Your Apps');
     });
--- test/support/commands.js
+++ test/support/commands.js
@@ -1,8 +1,26 @@
 Cypress.Commands.add('getByTestId', (testId) => cy.get(`[data-testid="${testId}"]`));
 Cypress.Commands.add('getByTitle', (title) => cy.get(`[title="${title}"]`));
-Cypress.Commands.add('showPreview', (token, fileId) => {
-    cy.get('[data-testid="token"]').type(token);
-    cy.get('[data-testid="token-set"]').click();
-    cy.get('[data-testid="fileid"]').type(fileId);
-    cy.get('[data-testid="fileid-set"]').click();
+Cypress.Commands.add('getPreviewPage', (pageNum) => {
+    cy
+        .get(`.page[data-page-number=${pageNum}]`)
+        .as('previewPage')
+        .find('[data-testid="page-loading-indicator"]')
+        .should('not.exist');
+
+    return cy.get('@previewPage');
+});
+Cypress.Commands.add('showPreview', (token, fileId, options) => {
+    cy.getByTestId('token').type(token);
+    cy.getByTestId('token-set').click();
+    cy.getByTestId('fileid').type(fileId);
+    cy.getByTestId('fileid-set').click();
+
+    if (options) {
+        cy.window().then((win) => {
+            win.loadPreview(options);
+        });
+    }
+
+    // Wait for .bp to load viewer
+    return cy.getByTestId('bp').should('have.class', 'bp-loaded');
 });
