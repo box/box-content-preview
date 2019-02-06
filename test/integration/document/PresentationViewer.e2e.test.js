a683f15054f5c40f276ea5cbb1654d0aabcd6fa6
Jeremy Press <jpress@box.com>
2019-02-06 11:10:52 -0800

    Chore: Adding e2e test and upgrading cypress

---
+++ test/integration/document/PresentationViewer.e2e.test.js
@@ -0,0 +1,35 @@
+// <reference types="Cypress" />
+describe('Preview Document Controls', () => {
+    const token = Cypress.env('ACCESS_TOKEN');
+    const fileId = Cypress.env('FILE_ID_PRESENTATION');
+
+    /* eslint-disable */
+    const showControls = () => {
+        cy.getByTestId('bp').trigger('mouseover');
+        cy.getByTestId('controls-wrapper').should('be.visible');
+    };
+    /* eslint-enable */
+
+    beforeEach(() => {
+        cy.visit('/');
+        cy.showPreview(token, fileId);
+        cy.getByTestId('current-page').as('currentPage');
+    });
+
+    it('Should initialize preview on the same page it was closed on', () => {
+        // Assert document content is present
+        cy.contains('For Teaching Economics');
+        // Get the current dimensions of the page
+        showControls();
+        // Navigate to the next page
+        cy.getByTitle('Next page').click();
+
+        // Refresh the preview
+        cy.reload();
+
+        // Page 2 should still be visible
+        cy.getPreviewPage(2).should('be.visible');
+
+    });
+
+});
