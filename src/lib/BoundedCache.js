a71d6cd7efc197243846c6b07dee559dcfc509e1
Conrad Chan <conradchan@box.com>
2019-02-07 14:39:09 -0800

    Chore: adding jsdoc

--- src/lib/BoundedCache.js
+++ src/lib/BoundedCache.js
@@ -1,10 +1,17 @@
 import Cache from './Cache';
 
 class BoundedCache extends Cache {
+    /** @property {Array} - Maintains the list of cache keys in order in which they were added to the cache */
     cacheQueue;
 
+    /** @property {number} - The maximum number of entries in the cache */
     maxEntries;
 
+    /**
+     * [constructor]
+     *
+     * @param {number} [maxEntries] - Override the maximum number of cache entries
+     */
     constructor(maxEntries) {
         super();
 
@@ -13,11 +20,25 @@ class BoundedCache extends Cache {
         this.cacheQueue = [];
     }
 
+    /**
+     * Destroys the bounded cache
+     *
+     * @return {void}
+     */
     destroy() {
         this.cache = null;
         this.cacheQueue = null;
     }
 
+    /**
+     * Caches a simple object in memory. If the number of cache entries
+     * then exceeds the maxEntries value, then the earliest key in cacheQueue
+     * will be removed from the cache.
+     *
+     * @param {string} key - The cache key
+     * @param {*} value - The cache value
+     * @return {void}
+     */
     set(key, value) {
         // If this key is not already in the cache, then add it
         // to the cacheQueue. This avoids adding the same key to
