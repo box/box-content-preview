2ed3c9b18d7640378d711adcded825977e138fd6
Jeremy Press <jpress@box.com>
2019-02-11 10:13:22 -0800

    Chore: Responding to PR comments

--- test/integration/document/PresentationViewer.e2e.test.js
+++ test/integration/document/PresentationViewer.e2e.test.js
@@ -3,12 +3,6 @@ describe('Presentation Viewer', () => {
     const token = Cypress.env('ACCESS_TOKEN');
     const fileId = Cypress.env('FILE_ID_PRESENTATION');
 
-    /* eslint-disable */
-    const showControls = () => {
-        cy.getByTestId('bp').trigger('mouseover');
-        cy.getByTestId('controls-wrapper').should('be.visible');
-    };
-    /* eslint-enable */
 
     beforeEach(() => {
         cy.visit('/');
@@ -18,15 +12,15 @@ describe('Presentation Viewer', () => {
     it('Should initialize preview on the same page it was closed on', () => {
         // Assert document content is present
         cy.contains('For Teaching Economics');
-        // Get the current dimensions of the page
-        showControls();
-        // Navigate to the next page
+
+        cy.showControls();
+
+        // Navigate to the second page so it gets cached
         cy.getByTitle('Next page').click();
 
-        // Refresh the preview
+        // Refreshes preview
         cy.reload();
 
-        // Page 2 should still be visible
         cy.getPreviewPage(2).should('be.visible');
 
     });
