70470d38b561a6aa8dd4ef7127f0a279cb71bd0d
Jeremy Press <jpress@box.com>
2019-04-12 16:38:30 -0700

    Docs: Add comment around cypress auth

--- test/support/constants.js
+++ test/support/constants.js
@@ -1,4 +1,5 @@
 Cypress.env({
+    // NOTE: The token used is a readonly token accessing public data in a demo enterprise.
     ACCESS_TOKEN: 'S8wjvjOL9GEK5VtXsQNVMOwSrx1g55oC',
     FILE_ID_DOC: '415542803939',
     FILE_ID_DOC_LARGE: '420985736453',
