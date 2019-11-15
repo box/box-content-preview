b21c5067b5181a7edcf420023c9c54dd896fc399
Conrad Chan <cjchan@gmail.com>
2019-11-15 13:14:01 -0800

    chore(controls): add e2e tests for toggle findbar and zoom (#1099)

--- test/integration/document/Controls.e2e.test.js
+++ test/integration/document/Controls.e2e.test.js
@@ -10,75 +10,121 @@ describe('Preview Document Controls', () => {
         cy.getPreviewPage(1);
     });
 
-    it('Should zoom in and out', () => {
-        // Assert document content is present
-        cy.contains('The Content Platform for Your Apps');
-        // Get the current dimensions of the page
-        cy.getPreviewPage(1).then(($page) => {
-            cy.wrap($page[0].scrollWidth).as('originalWidth');
-            cy.wrap($page[0].scrollHeight).as('originalHeight');
-        });
+    describe('Zoom controls', () => {
+        const zoom = inOrOut => {
+            cy.getByTestId('current-zoom')
+                .invoke('text')
+                .then(originalZoom => {
+                    cy.getByTitle(`Zoom ${inOrOut}`).click();
 
-        cy.showDocumentControls();
+                    cy.getByTestId('current-zoom')
+                        .invoke('text')
+                        .should('not.equal', originalZoom);
+                });
+        };
 
-        cy.getByTitle('Zoom out').click();
+        it('Should zoom in and out', () => {
+            // Assert document content is present
+            cy.contains('The Content Platform for Your Apps');
+            // Get the current dimensions of the page
+            cy.getPreviewPage(1).then($page => {
+                cy.wrap($page[0].scrollWidth).as('originalWidth');
+                cy.wrap($page[0].scrollHeight).as('originalHeight');
+            });
 
-        cy.getPreviewPage(1).then(($page) => {
-            const zoomedOutWidth = $page[0].scrollWidth;
-            const zoomedOutHeight = $page[0].scrollHeight;
+            cy.showDocumentControls();
 
-            cy.get('@originalWidth').then((originalWidth) => expect(originalWidth).to.be.above(zoomedOutWidth));
-            cy.get('@originalHeight').then((originalHeight) => expect(originalHeight).to.be.above(zoomedOutHeight));
+            zoom('out');
 
-            cy.wrap(zoomedOutWidth).as('zoomedOutWidth');
-            cy.wrap(zoomedOutHeight).as('zoomedOutHeight');
-        });
+            cy.getPreviewPage(1).then($page => {
+                const zoomedOutWidth = $page[0].scrollWidth;
+                const zoomedOutHeight = $page[0].scrollHeight;
 
-        cy.showDocumentControls();
+                cy.get('@originalWidth').then(originalWidth => expect(originalWidth).to.be.above(zoomedOutWidth));
+                cy.get('@originalHeight').then(originalHeight => expect(originalHeight).to.be.above(zoomedOutHeight));
 
-        cy.getByTitle('Zoom in').click();
+                cy.wrap(zoomedOutWidth).as('zoomedOutWidth');
+                cy.wrap(zoomedOutHeight).as('zoomedOutHeight');
+            });
 
-        cy.getPreviewPage(1).then(($page) => {
-            const zoomedInWidth = $page[0].scrollWidth;
-            const zoomedInHeight = $page[0].scrollHeight;
+            cy.showDocumentControls();
 
-            cy.get('@zoomedOutWidth').then((zoomedOutWidth) => expect(zoomedOutWidth).to.be.below(zoomedInWidth));
-            cy.get('@zoomedOutHeight').then((zoomedOutHeight) => expect(zoomedOutHeight).to.be.below(zoomedInHeight));
+            zoom('in');
+
+            cy.getPreviewPage(1).then($page => {
+                const zoomedInWidth = $page[0].scrollWidth;
+                const zoomedInHeight = $page[0].scrollHeight;
+
+                cy.get('@zoomedOutWidth').then(zoomedOutWidth => expect(zoomedOutWidth).to.be.below(zoomedInWidth));
+                cy.get('@zoomedOutHeight').then(zoomedOutHeight => expect(zoomedOutHeight).to.be.below(zoomedInHeight));
+            });
         });
     });
 
-    it('Should handle page navigation via buttons', () => {
-        cy.getPreviewPage(1).should('be.visible');
-        cy.contains('The Content Platform for Your Apps');
-        cy.get('@currentPage').invoke('text').should('equal', '1');
+    describe('Document page controls', () => {
+        it('Should handle page navigation via buttons', () => {
+            cy.getPreviewPage(1).should('be.visible');
+            cy.contains('The Content Platform for Your Apps');
+            cy.get('@currentPage')
+                .invoke('text')
+                .should('equal', '1');
 
-        cy.showDocumentControls();
-        cy.getByTitle('Next page').click();
+            cy.showDocumentControls();
+            cy.getByTitle('Next page').click();
 
-        cy.getPreviewPage(2).should('be.visible');
-        cy.contains('Discover how your business can use Box Platform');
-        cy.get('@currentPage').invoke('text').should('equal', '2');
+            cy.getPreviewPage(2).should('be.visible');
+            cy.contains('Discover how your business can use Box Platform');
+            cy.get('@currentPage')
+                .invoke('text')
+                .should('equal', '2');
 
-        cy.showDocumentControls();
-        cy.getByTitle('Previous page').click();
+            cy.showDocumentControls();
+            cy.getByTitle('Previous page').click();
 
-        cy.getPreviewPage(1).should('be.visible');
-        cy.contains('The Content Platform for Your Apps');
-        cy.get('@currentPage').invoke('text').should('equal', '1');
+            cy.getPreviewPage(1).should('be.visible');
+            cy.contains('The Content Platform for Your Apps');
+            cy.get('@currentPage')
+                .invoke('text')
+                .should('equal', '1');
+        });
+
+        it('Should handle page navigation via input', () => {
+            cy.getPreviewPage(1).should('be.visible');
+            cy.contains('The Content Platform for Your Apps');
+            cy.get('@currentPage')
+                .invoke('text')
+                .should('equal', '1');
+
+            cy.showDocumentControls();
+            cy.getByTitle('Click to enter page number').click();
+            cy.getByTestId('page-num-input')
+                .should('be.visible')
+                .type('2')
+                .blur();
+
+            cy.getPreviewPage(2).should('be.visible');
+            cy.contains('Discover how your business can use Box Platform');
+            cy.get('@currentPage')
+                .invoke('text')
+                .should('equal', '2');
+        });
     });
 
-    it('Should handle page navigation via input', () => {
-        cy.getPreviewPage(1).should('be.visible');
-        cy.contains('The Content Platform for Your Apps');
-        cy.get('@currentPage').invoke('text').should('equal', '1');
+    describe('Document find bar control', () => {
+        it('Should open the findbar', () => {
+            cy.getPreviewPage(1).should('be.visible');
+            cy.contains('The Content Platform for Your Apps');
 
-        cy.showDocumentControls();
-        cy.getByTitle('Click to enter page number').click();
-        cy.getByTestId('page-num-input').should('be.visible').type('2').blur();
+            cy.getByTestId('document-findbar').should('not.be.visible');
 
-        cy.getPreviewPage(2).should('be.visible');
-        cy.contains('Discover how your business can use Box Platform');
-        cy.get('@currentPage').invoke('text').should('equal', '2');
+            cy.showDocumentControls();
+            cy.getByTitle('Toggle findbar').click();
+            cy.getByTestId('document-findbar').should('be.visible');
+
+            cy.showDocumentControls();
+            cy.getByTitle('Toggle findbar').click();
+            cy.getByTestId('document-findbar').should('not.be.visible');
+        });
     });
 
     // Fullscreen won't allow a non user gesture to trigger fullscreen
