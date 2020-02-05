186c8d465c739114ab6ccf248a6d29b367cfdab8
Mingze Xiao <mxiao@box.com>
2020-02-05 13:07:02 -0800

    fix(archive): Address comments

--- test/integration/archive/ArchiveViewer.e2e.test.js
+++ test/integration/archive/ArchiveViewer.e2e.test.js
@@ -2,13 +2,14 @@
 describe('Archive Viewer', () => {
     const token = Cypress.env('ACCESS_TOKEN');
     const fileId = Cypress.env('FILE_ID_ARCHIVE');
+    const fileIdLong = Cypress.env('FILE_ID_ARCHIVE_LONG');
 
     beforeEach(() => {
         cy.visit('/');
-        cy.showPreview(token, fileId);
     });
 
     it('Should render correct item list', () => {
+        cy.showPreview(token, fileId);
         // clicking folder
         cy.getByTitle('Preview SDK Sample Archive').within(() => {
             cy.get('button').click();
@@ -23,6 +24,7 @@ describe('Archive Viewer', () => {
     });
 
     it('Should sort items when column header is clicked', () => {
+        cy.showPreview(token, fileId);
         cy.getByTitle('Preview SDK Sample Archive').within(() => {
             cy.get('button').click();
         });
@@ -39,6 +41,7 @@ describe('Archive Viewer', () => {
     });
 
     it('Should show matched item list based on search query', () => {
+        cy.showPreview(token, fileId);
         cy.getByTitle('Preview SDK Sample Archive').within(() => {
             cy.get('button').click();
         });
@@ -52,5 +55,31 @@ describe('Archive Viewer', () => {
         // fuzzy search
         cy.getByTestId('search-input').type('vido');
         cy.contains('Video (Normal).mp4');
+    });
+
+    it('Should reset scroll when navigating folders', () => {
+        cy.showPreview(token, fileIdLong);
+
+        cy.getByTitle('Collection').within(() => {
+            cy.get('button').click();
+        });
+
+        cy.get('.ReactVirtualized__Table__Grid').then($tables => {
+            const table = $tables[0];
+            table.scrollTo(0, 50);
+
+            const tableTop = table.getBoundingClientRect().top;
+            cy.get('.ReactVirtualized__Table__row').then($rows => {
+                expect($rows[0].getBoundingClientRect().top).not.to.equal(tableTop);
+            });
+
+            cy.getByTitle('Collection Child').within(() => {
+                cy.get('button').click();
+            });
+
+            cy.get('.ReactVirtualized__Table__row').then($rows => {
+                expect($rows[0].getBoundingClientRect().top).to.equal(tableTop);
+            });
+        });
     });
 });
