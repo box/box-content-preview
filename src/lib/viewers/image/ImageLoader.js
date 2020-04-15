8ca12d1e1db018e8cf581456df7d4cdea497cda3
Ian Kerr <ikerr@box.com>
2020-04-15 13:31:27 -0700

    feat(images): fixed issue with viewer selection, added tests

--- src/lib/viewers/image/ImageLoader.js
+++ src/lib/viewers/image/ImageLoader.js
@@ -72,6 +72,31 @@ class ImageLoader extends AssetLoader {
             return viewer.REP === entry.representation;
         });
     }
+
+    /**
+     * @inheritdoc
+     */
+    determineViewer(file, disabledViewers = []) {
+        return this.viewers.find(viewer => {
+            if (disabledViewers.indexOf(viewer.NAME) > -1 || viewer.EXT.indexOf(file.extension) === -1) {
+                return false;
+            }
+
+            const multiPageViewer = (viewer.ASSET || '').indexOf('{page}') !== -1;
+
+            return file.representations.entries.some(entry => {
+                if (viewer.REP !== entry.representation) {
+                    return false;
+                }
+
+                // If the viewer uses paged assets (e.g., image_2048_jpg), then
+                // ensure that there is a compatible paged representation.
+                const pagedRep = (entry.properties || {}).paged === 'true';
+
+                return !multiPageViewer || pagedRep;
+            });
+        });
+    }
 }
 
 export default new ImageLoader();
