96942d6647b1519bd689d460e7d4cd78a26c69ee
Mingze <mxiao@box.com>
2020-07-14 14:18:27 -0700

    feat(office): Add viewer option for max file size (#1235)
    * feat(office): Add viewer option for max file size
    
    * feat(office): change option name
    
    * feat(office): address PR feedback
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/lib/viewers/office/__tests__/OfficeLoader-test.js
+++ src/lib/viewers/office/__tests__/OfficeLoader-test.js
@@ -3,6 +3,8 @@ import OfficeViewer from '../OfficeViewer';
 import * as file from '../../../file';
 import { PERMISSION_DOWNLOAD } from '../../../constants';
 
+const FIVE_MB = 5242880;
+
 const sandbox = sinon.sandbox.create();
 
 describe('lib/viewers/office/OfficeLoader', () => {
@@ -43,9 +45,11 @@ describe('lib/viewers/office/OfficeLoader', () => {
             });
 
             it('should choose the Office viewer if it is not disabled and the file is a shared link that is not password-protected', () => {
-                const editedFakeFile = fakeFile;
-                editedFakeFile.shared_link = {
-                    is_password_enabled: false,
+                const editedFakeFile = {
+                    ...fakeFile,
+                    shared_link: {
+                        is_password_enabled: false,
+                    },
                 };
                 const viewer = OfficeLoader.determineViewer(editedFakeFile);
                 expect(viewer.NAME).to.equal('Office');
@@ -57,8 +61,7 @@ describe('lib/viewers/office/OfficeLoader', () => {
             });
 
             it('should not return a viewer if the file is too large', () => {
-                const editedFakeFile = fakeFile;
-                editedFakeFile.size = 5242881;
+                const editedFakeFile = { ...fakeFile, size: FIVE_MB + 1 };
                 const viewer = OfficeLoader.determineViewer(editedFakeFile, []);
                 expect(viewer).to.equal(undefined);
             });
@@ -73,12 +76,25 @@ describe('lib/viewers/office/OfficeLoader', () => {
             });
 
             it('should not return a viewer if the file is a password-protected shared link', () => {
-                const editedFakeFile = fakeFile;
-                editedFakeFile.shared_link = {
-                    is_password_enabled: true,
+                const editedFakeFile = {
+                    ...fakeFile,
+                    shared_link: {
+                        is_password_enabled: true,
+                    },
                 };
                 const viewer = OfficeLoader.determineViewer(editedFakeFile, []);
                 expect(viewer).to.equal(undefined);
+            });
+
+            it('should respect maxFileSize in viewerOptions', () => {
+                const viewerOptions = {
+                    Office: {
+                        maxFileSize: FIVE_MB + 1,
+                    },
+                };
+                const editedFakeFile = { ...fakeFile, size: FIVE_MB + 1 };
+                const viewer = OfficeLoader.determineViewer(editedFakeFile, [], viewerOptions);
+                expect(viewer).to.not.equal(undefined);
             });
         });
     });
