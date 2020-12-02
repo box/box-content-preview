cd5bbb91fade20b95df898e692f7ca363d8d0661
Jared Stoffan <jstoffan@box.com>
2020-12-02 15:57:58 -0800

    feat(controls): Add react versions of existing media control icons

---
+++ src/lib/viewers/controls/icons/IconPause24.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+function IconPause24(props: React.SVGProps<SVGSVGElement>): JSX.Element {
+    return (
+        <svg fill="#fff" focusable={false} height={24} width={24} {...props}>
+            <path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z" />
+            <path d="M0 0h24v24H0z" fill="none" />
+        </svg>
+    );
+}
+
+export default IconPause24;
---
+++ src/lib/viewers/controls/icons/IconVolumeLow24.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+function IconVolumeLow24(props: React.SVGProps<SVGSVGElement>): JSX.Element {
+    return (
+        <svg focusable={false} height={24} viewBox="-3 5 24 24" width={24} {...props}>
+            <path d="M0 14v6h4l5 5V9l-5 5H0z" fill="#FFF" />
+            <path d="M-3 5h24v24H-3V5z" fill="none" />
+        </svg>
+    );
+}
+
+export default IconVolumeLow24;
