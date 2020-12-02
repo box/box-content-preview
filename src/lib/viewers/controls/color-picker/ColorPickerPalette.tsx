83187a62caf84c352a603e5eaa7f3ab0221d59fd
Jack Chen <yuxi.chen@berkeley.edu>
2020-12-02 15:13:39 -0800

    chore(drawing): clean up styles / unused imports

--- src/lib/viewers/controls/color-picker/ColorPickerPalette.tsx
+++ src/lib/viewers/controls/color-picker/ColorPickerPalette.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import noop from 'lodash/noop';
-
 import ControlsBar from '../controls-bar';
-
 import './ColorPickerPalette.scss';
 
 export type Props = {
     onColorSelect: () => void; // TODO: this method wil need to take in a color string param
 };
 
-export default function ColorPickerPalette({ onColorSelect = noop }: Props): JSX.Element {
+export default function ColorPickerPalette({ onColorSelect }: Props): JSX.Element {
     const colors = ['#0061d5', '#26c281', '#ed3757', '#f5b31b', '#ffd700', '#4826c2'];
 
     return (
