071ed548519de1df5a7fbc9f49a9193ccc92861e
Jack Chen <yuxi.chen@berkeley.edu>
2020-12-08 15:18:47 -0800

    chore(drawing): test getters / setters

--- src/lib/__tests__/AnnotationModule-test.js
+++ src/lib/__tests__/AnnotationModule-test.js
@@ -1,4 +1,5 @@
 import { bdlBoxBlue } from 'box-ui-elements/es/styles/variables';
+import Cache from '../Cache';
 import AnnotationModule, { ANNOTATION_COLOR_KEY } from '../AnnotationModule';
 
 describe('lib/AnnotationModule', () => {
@@ -12,5 +13,22 @@ describe('lib/AnnotationModule', () => {
 
         expect(annotationModule.cache.get).toHaveBeenCalledWith(ANNOTATION_COLOR_KEY);
         expect(annotationModule.cache.set).toHaveBeenCalledWith(ANNOTATION_COLOR_KEY, bdlBoxBlue, true);
+    });
+
+    test('getColor', () => {
+        const cache = new Cache();
+        const annotationModule = new AnnotationModule({ cache });
+
+        expect(annotationModule.getColor()).toBe(bdlBoxBlue);
+    });
+
+    test('setColor', () => {
+        const cache = new Cache();
+        const color = '#fff';
+        const annotationModule = new AnnotationModule({ cache });
+
+        annotationModule.setColor(color);
+
+        expect(annotationModule.getColor()).toBe(color);
     });
 });
