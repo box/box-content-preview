f234f244e076c4135526acd2d1731a7d36a6a0d3
Jack Chen <yuxi.chen@berkeley.edu>
2020-12-08 09:20:47 -0800

    chore(drawing): revert react import

--- src/lib/viewers/controls/annotations/__tests__/AnnotationsControls-test.tsx
+++ src/lib/viewers/controls/annotations/__tests__/AnnotationsControls-test.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { act } from 'react-dom/test-utils';
 import { ReactWrapper, mount } from 'enzyme';
 import AnnotationsControls from '../AnnotationsControls';
