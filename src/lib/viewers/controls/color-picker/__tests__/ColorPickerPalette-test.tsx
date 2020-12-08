274e2cbbbc4e041a1fc2365d73e50184940cea41
Jack Chen <yuxi.chen@berkeley.edu>
2020-12-08 11:28:45 -0800

    chore(drawing): make ColorPickerPalette annotations agnostic

--- src/lib/viewers/controls/color-picker/__tests__/ColorPickerPalette-test.tsx
+++ src/lib/viewers/controls/color-picker/__tests__/ColorPickerPalette-test.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import { shallow, ShallowWrapper } from 'enzyme';
 import ColorPickerPalette from '../ColorPickerPalette';
-import { AnnotationColor } from '../../../../AnnotationModule';
 
 describe('ColorPickerPalette', () => {
-    const colors = Object.values(AnnotationColor);
+    const colors = ['#fff'];
 
     const getWrapper = (props = {}): ShallowWrapper =>
         shallow(<ColorPickerPalette colors={colors} onSelect={jest.fn()} {...props} />);
 
     describe('render', () => {
-        test('should render the six color swatches', () => {
+        test('should render a single color swatch', () => {
             const wrapper = getWrapper();
 
-            expect(wrapper.find('button').length).toBe(6);
+            expect(wrapper.find('button').length).toBe(1);
         });
     });
 });
