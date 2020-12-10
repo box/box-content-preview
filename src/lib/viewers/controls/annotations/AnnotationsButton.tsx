5e2b98aa0cad86e18a06e0b9fa03ac155c2227a6
Jared Stoffan <jstoffan@box.com>
2020-12-09 17:53:46 -0800

    feat(controls): Add react versions of core media controls

--- src/lib/viewers/controls/annotations/AnnotationsButton.tsx
+++ src/lib/viewers/controls/annotations/AnnotationsButton.tsx
@@ -1,10 +1,10 @@
-import React, { ButtonHTMLAttributes } from 'react';
+import React from 'react';
 import classNames from 'classnames';
 import noop from 'lodash/noop';
 import { AnnotationMode } from './types';
 import './AnnotationsButton.scss';
 
-export type Props = Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'onClick'> & {
+export type Props = Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'onClick'> & {
     children?: React.ReactNode;
     className?: string;
     isActive?: boolean;
--- src/lib/viewers/controls/hooks/index.ts
+++ src/lib/viewers/controls/hooks/index.ts
@@ -1,2 +1,2 @@
-// eslint-disable-next-line import/prefer-default-export
 export { default as useFullscreen } from './useFullscreen';
+export { default as usePreventKey } from './usePreventKey';
