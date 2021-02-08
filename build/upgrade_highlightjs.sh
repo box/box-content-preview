d098ab7a21626b2325240c753e57427d9a54f723
Conrad Chan <conradchan@box.com>
2021-02-08 13:58:30 -0800

    chore: pr comments

--- build/upgrade_highlightjs.sh
+++ build/upgrade_highlightjs.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # Run with specific branch/tag (e.g. ./upgrade_highlightjs.sh 10.6.0) or with no arguments to use master
 
-# TODO: This list of languages should be made more dynamic based on the file extensions Preview supports
+# TODO: This list of languages should be made more dynamic based on the file extensions Preview supports, see src/lib/extensions.js
+HIGHLIGHTJS_DIST="highlight.pack.js"
 HIGHLIGHTJS_LANGUAGES="actionscript dos c cpp cmake csharp css diff erb groovy haml java javascript json less makefile ocaml php perl properties python ruby scss scala sml sql bash vim yaml"
+HIGHLIGHTJS_SRC_DIR="highlightjs"
 TEXT_BASE_PATH="src/third-party/text"
 TEXT_STATIC_ASSETS_BRANCH=${1:-master}
 TEXT_STATIC_ASSETS_VERSION=$(./build/current_version.sh)
@@ -12,16 +14,21 @@ build_custom_highlightjs() {
     echo "-----------------------------------------------------------------------------------"
     echo "Cloning higlightjs repo at branch: $TEXT_STATIC_ASSETS_BRANCH..."
     echo "-----------------------------------------------------------------------------------"
-    rm -rf ./highlight.js
-    git clone git@github.com:highlightjs/highlight.js.git --depth 1 --single-branch --branch ${TEXT_STATIC_ASSETS_BRANCH} || return 1
+    rm -rf ${HIGHLIGHTJS_SRC_DIR}
+    git clone git@github.com:highlightjs/highlight.js.git --depth 1 --single-branch --branch ${TEXT_STATIC_ASSETS_BRANCH} ${HIGHLIGHTJS_SRC_DIR} || return 1
 
     echo "-----------------------------------------------------------------------------------"
     echo "Building higlightjs for browser for languages ${HIGHLIGHTJS_LANGUAGES}..."
     echo "-----------------------------------------------------------------------------------"
-    cd ./highlight.js
-    yarn install || return 1
-    node tools/build -t browser ${HIGHLIGHTJS_LANGUAGES} || return 1
-    cd ../
+    # Use subshell to execute the custom build to avoid having to keep track of the current working directory
+    (cd ${HIGHLIGHTJS_SRC_DIR} && yarn install --frozen-lockfile && node tools/build -t browser ${HIGHLIGHTJS_LANGUAGES}) || return 1
+}
+
+cleanup_custom_highlightjs() {
+    echo "-----------------------------------------------------------------------------------"
+    echo "Cleaning up highlightjs repo"
+    echo "-----------------------------------------------------------------------------------"
+    rm -rf ${HIGHLIGHTJS_SRC_DIR}
 }
 
 prepare_target_directory() {
@@ -33,36 +40,24 @@ prepare_target_directory() {
     TEXT_CURRENT_ASSETS_VERSIONS=`ls ${TEXT_BASE_PATH} | sort -t "." -k1,1n -k2,2n -k3,3n | tail -1`
 
     echo "Using base version from $TEXT_CURRENT_ASSETS_VERSIONS"
-    mkdir ${TEXT_STATIC_ASSETS_PATH}
-    cp -p ${TEXT_BASE_PATH}/${TEXT_CURRENT_ASSETS_VERSIONS}/* ${TEXT_STATIC_ASSETS_PATH}/
+    mkdir -v ${TEXT_STATIC_ASSETS_PATH}
+    cp -pv ${TEXT_BASE_PATH}/${TEXT_CURRENT_ASSETS_VERSIONS}/* ${TEXT_STATIC_ASSETS_PATH}/
 }
 
 process_highlightjs_assets() {
     echo "-----------------------------------------------------------------------------------"
     echo "Copying relevant files to third-party directory..."
     echo "-----------------------------------------------------------------------------------"
-    cp highlight.js/build/highlight.min.js ${TEXT_STATIC_ASSETS_PATH}
-    cp highlight.js/src/styles/github.css ${TEXT_STATIC_ASSETS_PATH}
+    cp -v ${HIGHLIGHTJS_SRC_DIR}/build/${HIGHLIGHTJS_DIST} ${TEXT_STATIC_ASSETS_PATH}/highlight.min.js || return 1
+    cp -v ${HIGHLIGHTJS_SRC_DIR}/src/styles/github.css ${TEXT_STATIC_ASSETS_PATH} || return 1
 
     echo "-----------------------------------------------------------------------------------"
     echo "Minifying github.css with cssnano"
     echo "-----------------------------------------------------------------------------------"
     ./node_modules/.bin/cssnano ${TEXT_STATIC_ASSETS_PATH}/github.css ${TEXT_STATIC_ASSETS_PATH}/github.min.css || return 1
-
-    echo "-----------------------------------------------------------------------------------"
-    echo "Cleaning up highlightjs repo"
-    echo "-----------------------------------------------------------------------------------"
-    rm -rf ./highlight.js
 }
 
 upgrade_highlightjs() {
-    if [[ $(git diff --shortstat 2> /dev/null | tail -n1) != "" ]] ; then
-       echo "----------------------------------------------------"
-       echo "Your branch is dirty!"
-       echo "----------------------------------------------------"
-       exit 1
-    fi
-
     echo "Upgrading highlight.js to $TEXT_STATIC_ASSETS_BRANCH";
 
     # Prepare target directory under third-party/text
@@ -73,6 +68,9 @@ upgrade_highlightjs() {
 
     # Copy over built assets to target directory
     process_highlightjs_assets || return 1
+
+    # Cleanup highlightjs
+    cleanup_custom_highlightjs || return 1
 
     echo "-----------------------------------------------------------------------------------"
     echo "Successfully upgraded highlightjs! 🚀"
@@ -86,7 +84,7 @@ if ! upgrade_highlightjs; then
     echo "----------------------------------------------------------------------"
 
     echo "----------------------------------------------------------------------"
-    echo "Cleaning workspace by deleting ${TEXT_STATIC_ASSETS_PATH} and ./highlight.js"
+    echo "Clean workspace by deleting ${TEXT_STATIC_ASSETS_PATH} and ${HIGHLIGHTJS_SRC_DIR}"
     echo "----------------------------------------------------------------------"
     exit 1
 fi
