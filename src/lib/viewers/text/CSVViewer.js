9e66b55be3ff2e6e1530371bb1357f1718c5a92e
Conrad Chan <conradchan@box.com>
2021-02-24 15:52:33 -0800

    build(deps): Upgrade Papaparse to 5.0.0 (#1330)

--- src/lib/viewers/text/CSVViewer.js
+++ src/lib/viewers/text/CSVViewer.js
@@ -1,9 +1,8 @@
+import PreviewError from '../../PreviewError';
 import TextBaseViewer from './TextBaseViewer';
-import { createAssetUrlCreator } from '../../util';
+import { ERROR_CODE, VIEWER_EVENT } from '../../events';
 import { TEXT_STATIC_ASSETS_VERSION } from '../../constants';
 import './CSV.scss';
-import { ERROR_CODE, VIEWER_EVENT } from '../../events';
-import PreviewError from '../../PreviewError';
 
 const JS = [`third-party/text/${TEXT_STATIC_ASSETS_VERSION}/papaparse.min.js`, 'csv.js'];
 const PAPAPARSE_ERROR_TYPES = {
@@ -53,38 +52,30 @@ class CSVViewer extends TextBaseViewer {
     load() {
         super.load();
 
-        const { representation, location } = this.options;
+        const { representation } = this.options;
         const template = representation.content.url_template;
-        const assetUrlCreator = createAssetUrlCreator(location);
-        const papaWorkerUrl = assetUrlCreator(JS[0]);
 
         return Promise.all([this.loadAssets(JS), this.getRepStatus().getPromise()])
             .then(() => {
-                this.api.get(papaWorkerUrl, { type: 'blob' }).then(papaWorkerBlob => {
-                    /* global Papa */
-                    const workerSrc = URL.createObjectURL(papaWorkerBlob);
-                    Papa.SCRIPT_PATH = workerSrc;
+                this.startLoadTimer();
+                const urlWithAuth = this.createContentUrlWithAuthParams(template);
+                Papa.parse(urlWithAuth, {
+                    download: true,
+                    error: (err, file, inputElem, reason) => {
+                        const error = new PreviewError(ERROR_CODE.LOAD_CSV, __('error_refresh'), { reason });
+                        this.handleDownloadError(error, urlWithAuth);
+                    },
+                    complete: results => {
+                        if (this.isDestroyed() || !results) {
+                            return;
+                        }
 
-                    this.startLoadTimer();
-                    const urlWithAuth = this.createContentUrlWithAuthParams(template);
-                    Papa.parse(urlWithAuth, {
-                        download: true,
-                        error: (err, file, inputElem, reason) => {
-                            const error = new PreviewError(ERROR_CODE.LOAD_CSV, __('error_refresh'), { reason });
-                            this.handleDownloadError(error, urlWithAuth);
-                        },
-                        complete: results => {
-                            if (this.isDestroyed() || !results) {
-                                return;
-                            }
+                        this.checkForParseErrors(results);
 
-                            this.checkForParseErrors(results);
-
-                            this.data = results.data;
-                            this.finishLoading();
-                            URL.revokeObjectURL(workerSrc);
-                        },
-                    });
+                        this.data = results.data;
+                        this.finishLoading();
+                    },
+                    worker: true,
                 });
             })
             .catch(this.handleAssetError);
