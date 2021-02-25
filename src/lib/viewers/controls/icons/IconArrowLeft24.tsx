ca8eb8525f522a79ff872074b5e14bdc8561c32f
Mingze Xiao <mxiao@box.com>
2021-02-25 13:45:59 -0800

    feat(controls): Add Settings Controls icons

---
+++ src/lib/viewers/controls/icons/IconArrowLeft24.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+function IconArrowLeft24(props: React.SVGProps<SVGSVGElement>): JSX.Element {
+    return (
+        <svg fill="#999EA4" focusable="false" height={24} viewBox="0 0 24 24" width={24} {...props}>
+            <path d="M15.41 16.09l-4.58-4.59 4.58-4.59L14 5.5l-6 6 6 6z" />
+            <path d="M0-.5h24v24H0z" fill="none" />
+        </svg>
+    );
+}
+
+export default IconArrowLeft24;
