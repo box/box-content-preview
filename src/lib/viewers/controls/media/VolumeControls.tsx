5de125220a3e864b7d8406a4f75eb2dc10dfeee5
Jared Stoffan <jstoffan@users.noreply.github.com>
2021-03-12 11:15:18 -0800

    chore(deps): Upgrade React dependencies to v17 (#1334)

--- src/lib/viewers/controls/media/VolumeControls.tsx
+++ src/lib/viewers/controls/media/VolumeControls.tsx
@@ -6,9 +6,9 @@ import IconVolumeLow24 from '../icons/IconVolumeLow24';
 import IconVolumeMedium24 from '../icons/IconVolumeMedium24';
 import IconVolumeMute24 from '../icons/IconVolumeMute24';
 import MediaToggle from './MediaToggle';
-import SliderControl, { Ref as SliderControlRef } from '../slider';
+import SliderControl from '../slider';
 import useAttention from '../hooks/useAttention';
-import usePreventKey from '../hooks/usePreventKey';
+import { decodeKeydown } from '../../../util';
 import './VolumeControls.scss';
 
 export type Props = {
@@ -33,11 +33,19 @@ export function getIcon(volume: number): (props: React.SVGProps<SVGSVGElement>) 
 
 export default function VolumeControls({ onMuteChange, onVolumeChange, volume = 1 }: Props): JSX.Element {
     const [isActive, handlers] = useAttention();
-    const inputElRef = React.useRef<SliderControlRef>(null);
     const isMuted = !volume;
     const Icon = isMuted ? IconVolumeMute24 : getIcon(volume);
     const title = isMuted ? __('media_unmute') : __('media_mute');
     const value = Math.round(volume * 100);
+
+    const handleKeydown = (event: React.KeyboardEvent): void => {
+        const key = decodeKeydown(event);
+
+        // Allow the range input to handle its own left/right keydown events
+        if (key === 'ArrowLeft' || key === 'ArrowRight') {
+            event.stopPropagation(); // Prevents global key handling
+        }
+    };
 
     const handleMute = (): void => {
         onMuteChange(!isMuted);
@@ -47,9 +55,6 @@ export default function VolumeControls({ onMuteChange, onVolumeChange, volume = 
         onVolumeChange(newValue / 100);
     };
 
-    // Allow the range input to handle its own left/right keydown events
-    usePreventKey(inputElRef, ['ArrowLeft', 'ArrowRight']);
-
     return (
         <div className="bp-VolumeControls">
             <MediaToggle className="bp-VolumeControls-toggle" onClick={handleMute} title={title} {...handlers}>
@@ -58,9 +63,9 @@ export default function VolumeControls({ onMuteChange, onVolumeChange, volume = 
 
             <div className={classNames('bp-VolumeControls-flyout', { 'bp-is-open': isActive })}>
                 <SliderControl
-                    ref={inputElRef}
                     className="bp-VolumeControls-slider"
                     onChange={handleVolume}
+                    onKeyDown={handleKeydown}
                     title={__('media_volume_slider')}
                     track={`linear-gradient(to right, ${bdlBoxBlue} ${value}%, ${white} ${value}%)`}
                     value={value}
