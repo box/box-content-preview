4414d11637ddb6481bf1c42f48e6cb787440617f
Hormaza <matiasbecerra@box.com>
2021-09-20 16:15:43 -0300

    fix(findbar): remove unnesesary hook adding event.target

--- src/lib/viewers/controls/findbar/FindBarToggle.tsx
+++ src/lib/viewers/controls/findbar/FindBarToggle.tsx
@@ -3,21 +3,18 @@ import IconSearch24 from '../icons/IconSearch24';
 import './FindBarToggle.scss';
 
 export type Props = {
-    onFindBarToggle?: (buttonElement: HTMLDivElement | null) => void;
+    onFindBarToggle?: (buttonElement: EventTarget | null) => void;
 };
 
 export default function FindBarToggle({ onFindBarToggle }: Props): JSX.Element | null {
-    const buttonReference = React.useRef(null);
-
     if (!onFindBarToggle) {
         return null;
     }
 
     return (
         <button
-            ref={buttonReference}
             className="bp-FindBarToggle"
-            onClick={(): void => onFindBarToggle(buttonReference.current)}
+            onClick={(event): void => onFindBarToggle(event.target)}
             title={__('toggle_findbar')}
             type="button"
         >
