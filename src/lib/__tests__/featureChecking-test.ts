b6d83d0843312ce6f592bde9942bffb42c0a9b7b
Jackie Chan <jackiechan@box.com>
2023-06-01 16:36:45 -0700

    feat(feature-toggles): Fix test

--- src/lib/__tests__/featureChecking-test.ts
+++ src/lib/__tests__/featureChecking-test.ts
@@ -9,23 +9,25 @@ const features = {
     },
 };
 
-describe('isFeatureEnabled', () => {
-    test('returns truthy when object is enabled and falsy when object is disabled', () => {
-        expect(isFeatureEnabled(features, 'shouldUseCar.enabled')).toBe(true);
-        expect(isFeatureEnabled(features, 'shouldUsePlane.enabled')).toBe(false);
-    });
-    test('defaults to false if object does not exist', () => {
-        expect(isFeatureEnabled(features, 'unknownKey')).toBe(false);
-    });
-});
-
-describe('getFeatureConfig', () => {
-    test('returns feature object if key exists', () => {
-        expect(getFeatureConfig(features, 'shouldUseCar')).toEqual({ enabled: true });
-        expect(getFeatureConfig(features, 'shouldUsePlane')).toEqual({ enabled: false });
+describe('featureChecking', () => {
+    describe('isFeatureEnabled', () => {
+        test('returns truthy when object is enabled and falsy when object is disabled', () => {
+            expect(isFeatureEnabled(features, 'shouldUseCar.enabled')).toBe(true);
+            expect(isFeatureEnabled(features, 'shouldUsePlane.enabled')).toBe(false);
+        });
+        test('defaults to false if object does not exist', () => {
+            expect(isFeatureEnabled(features, 'unknownKey')).toBe(false);
+        });
     });
 
-    test('defaults to empty object', () => {
-        expect(getFeatureConfig({}, 'unknownKey')).toEqual({});
+    describe('getFeatureConfig', () => {
+        test('returns feature object if key exists', () => {
+            expect(getFeatureConfig(features, 'shouldUseCar')).toEqual({ enabled: true });
+            expect(getFeatureConfig(features, 'shouldUsePlane')).toEqual({ enabled: false });
+        });
+
+        test('defaults to empty object', () => {
+            expect(getFeatureConfig({}, 'unknownKey')).toEqual({});
+        });
     });
 });
