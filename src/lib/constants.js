b7302d62f68783511c38cdec69683b937d3b74e3
Jackie Chan <jackiechan@box.com>
2023-06-01 16:39:28 -0700

    fix(e2e-tests): Update doc assets to 2.90

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -102,7 +102,7 @@ export const PDFJS_WIDTH_PADDING_PX = 40; // Should match SCROLLBAR_PADDING in p
 export const PDFJS_HEIGHT_PADDING_PX = 5; // Should match VERTICAL_PADDING in pdf_viewer.js
 
 export const DOC_LEGACY_STATIC_ASSETS_VERSION = '2.76.0'; // IE11 only
-export const DOC_STATIC_ASSETS_VERSION = '2.93.0'; // Modern browsers
+export const DOC_STATIC_ASSETS_VERSION = '2.90.0'; // Modern browsers
 export const MEDIA_STATIC_ASSETS_VERSION = '2.93.0';
 export const MODEL3D_STATIC_ASSETS_VERSION = '1.12.0';
 export const SWF_STATIC_ASSETS_VERSION = '0.112.0';
