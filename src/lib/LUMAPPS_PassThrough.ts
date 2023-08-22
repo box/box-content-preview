61bf2a04eefc6b3321804b1ea12720578a958c06
Eudes Boullie <eudes@lumapps.com>
2023-08-22 11:21:53 +0200

    feat(box content preview): js to ts EIN-2249

--- src/lib/LUMAPPS_PassThrough.ts
+++ src/lib/LUMAPPS_PassThrough.ts
@@ -1,8 +1,14 @@
 import axios from 'axios';
 // eslint-disable-next-line prettier/prettier
-// import type { LumAppsContext, PassThroughData } from './LUMAPPS_types';
+import type { LumAppsContext, PassThroughData } from './LUMAPPS_types';
 
-const lumappsPassThrough = async (LumAppsContext, connectorId, PassThroughData) => {
+
+const lumappsPassThrough = async (
+    LumAppsContext: LumAppsContext,
+    connectorId: string,
+    // connectorId: string ='f162a4ff-810b-43a7-a0f8-6888d25e37e2',
+    PassThroughData: PassThroughData,
+) => {
     const { baseUrl, haussmannCell, organizationId, token } = LumAppsContext;
 
     // const haussmannCell = 'https://go-cell-001.beta.api.lumapps.com'
@@ -16,4 +22,4 @@ const lumappsPassThrough = async (LumAppsContext, connectorId, PassThroughData) 
         { headers: { Authorization: `Bearer ${token}` } },
     );
 };
-export default lumappsPassThrough;
+export default lumappsPassThrough
