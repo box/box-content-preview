401c8f46b8d057cda647f083ce50afc1d48662c7
bfoxx1906 <99675599+bfoxx1906@users.noreply.github.com>
2023-08-31 12:02:40 -0400

    fix(pdf): fix disappearing form data (#1503)
    * feat(upgrade pdjs fix): update pdfjs upgrade script
    
    * feat(upgrade pdjs fix): update pdfjs upgrade script
    
    * fix(pdf): fix disappearing form text issue
    
    * fix(pdf): upgrading verify custom updates script
    
    * fix(pdf): adding sed command to new upgrade pdf.js script
    
    * fix(pdf): adding to new upgrade pdf.js script
    
    * fix(pdf): removing upgrade script
    
    * fix(pdf): reverting non pdf related changes

---
+++ build/verify_custom_updates.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+VERSION="`grep -o 'DOC_STATIC_ASSETS_VERSION.*' ./src/lib/constants.js | grep -o "'.*'" | sed -e "s/'//g"`"
+PIXEL_FIX_MISSING="`grep -c '.PixelsPerInch.PDF_TO_CSS_UNITS);return void 0!==' ./src/third-party/doc/$VERSION/pdf.min.js`"
+READ_ONLY_FIELD_FIX="`grep -c 'class TextWidgetAnnotation extends WidgetAnnotation{constructor(e){super(e);this.data.hasOwnCanvas=this.data.readOnly' ./src/third-party/doc/$VERSION/pdf.worker.min.js`"
+FAILED=false
+if [[ $PIXEL_FIX_MISSING != 0 ]];then
+    echo "Pixelation fix not implemented. See https://github.com/box/box-content-preview/pull/1466"
+    FAILED=true
+fi
+
+if [[ $READ_ONLY_FIELD_FIX != 0 ]];then
+    echo "-----------------------------------------"
+    echo "Read only form field fix not implemented."
+    echo "Run: sed -i '' -e 's/class TextWidgetAnnotation extends WidgetAnnotation{constructor(e){super(e);this.data.hasOwnCanvas=this.data.readOnly&&!this.data.noHTML/class TextWidgetAnnotation extends WidgetAnnotation{constructor(e){super(e);this.data.hasOwnCanvas=false/' ./src/third-party/doc/<version>/pdf.worker.min.js"
+    echo "------------------------------------------"
+    FAILED=true
+fi
+
+if [[ $FAILED == true ]];then
+exit 1
+fi
+
+exit 0
