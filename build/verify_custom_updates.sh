ea091ddb434f89825283eea42c8515688c00c218
Adam Horowitz <ahorowitz@box.com>
2024-05-21 13:00:56 -0400

    feat(pdfjs): upgrade pdj.js

--- build/verify_custom_updates.sh
+++ build/verify_custom_updates.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 VERSION="`grep -o 'DOC_STATIC_ASSETS_VERSION.*' ./src/lib/constants.js | grep -o "'.*'" | sed -e "s/'//g"`"
-if  [[ ${VERSION:0:1} -ge "4" ]]; then JS_FILE_EXTENSION="mjs"; else JS_FILE_EXTENSION="js"; fi
-PIXEL_FIX_MISSING="`grep -c '.PixelsPerInch.PDF_TO_CSS_UNITS);return void 0!==' ./src/third-party/doc/$VERSION/pdf.min.${JS_FILE_EXTENSION}`"
-READ_ONLY_FIELD_FIX="`grep -c 'class TextWidgetAnnotation extends WidgetAnnotation{constructor(e){super(e);this.data.hasOwnCanvas=this.data.readOnly' ./src/third-party/doc/$VERSION/pdf.worker.min.${JS_FILE_EXTENSION}`"
+PIXEL_FIX_MISSING="`grep -c '.PixelsPerInch.PDF_TO_CSS_UNITS);return void 0!==' ./src/third-party/doc/$VERSION/pdf.min.mjs`"
+READ_ONLY_FIELD_FIX="`grep -c 'class TextWidgetAnnotation extends WidgetAnnotation{constructor(e){super(e);this.data.hasOwnCanvas=this.data.readOnly' ./src/third-party/doc/$VERSION/pdf.worker.min.mjs`"
 FAILED=false
 if [[ $PIXEL_FIX_MISSING != 0 ]];then
     echo "Pixelation fix not implemented. See https://github.com/box/box-content-preview/pull/1466"
@@ -13,7 +12,7 @@ fi
 if [[ $READ_ONLY_FIELD_FIX != 0 ]];then
     echo "-----------------------------------------"
     echo "Read only form field fix not implemented."
-    echo "Run: sed -i '' -e 's/class TextWidgetAnnotation extends WidgetAnnotation{constructor(e){super(e);this.data.hasOwnCanvas=this.data.readOnly&&!this.data.noHTML/class TextWidgetAnnotation extends WidgetAnnotation{constructor(e){super(e);this.data.hasOwnCanvas=false/' ./src/third-party/doc/<version>/pdf.worker.min.js"
+    echo "Run: sed -i '' -e 's/class TextWidgetAnnotation extends WidgetAnnotation{constructor(e){super(e);this.data.hasOwnCanvas=this.data.readOnly&&!this.data.noHTML/class TextWidgetAnnotation extends WidgetAnnotation{constructor(e){super(e);this.data.hasOwnCanvas=false/' ./src/third-party/doc/<version>/pdf.worker.min.mjs"
     echo "------------------------------------------"
     FAILED=true
 fi
