d3d81ab34945f3df00150c19af38c3f5a48518b1
Adam Horowitz <ahorowitz@box.com>
2024-05-23 15:16:40 -0400

    feat(pdfjs): upgrade pdj.js

--- build/upgrade_pdfjs.sh
+++ build/upgrade_pdfjs.sh
@@ -11,47 +11,48 @@ DOC_STATIC_ASSETS_PATH="src/third-party/doc/${DOC_STATIC_ASSETS_VERSION}"
 echo "Upgrading pdf.js to $DOC_STATIC_ASSETS_BRANCH";
 
 echo "-----------------------------------------------------------------------------------"
-echo "Creating target directory at $DOC_STATIC_ASSETS_PATH..."
+echo "Cleaning up existing upgrade files/folders if they exist..."
 echo "-----------------------------------------------------------------------------------"
-
 rm -rf ${DOC_STATIC_ASSETS_PATH}
 rm -rf pdfjs-dist
 rm pdfjs-dist.zip
+
 DOC_CURRENT_ASSETS_VERSIONS=`ls src/third-party/doc | sort -t "." -k1,1n -k2,2n -k3,3n | tail -1`
 
-echo "Using base version from $DOC_CURRENT_ASSETS_VERSIONS"
+echo "-----------------------------------------------------------------------------------"
+echo "Creating target directory at $DOC_STATIC_ASSETS_PATH..."
+echo "-----------------------------------------------------------------------------------"
 mkdir ${DOC_STATIC_ASSETS_PATH}
+
+echo "-----------------------------------------------------------------------------------"
+echo "Copying legacy files/folders from version ${DOC_CURRENT_ASSETS_VERSIONS}..."
+echo "-----------------------------------------------------------------------------------"
 \cp -R src/third-party/doc/${DOC_CURRENT_ASSETS_VERSIONS}/exif.js ${DOC_STATIC_ASSETS_PATH}/
 \cp -R src/third-party/doc/${DOC_CURRENT_ASSETS_VERSIONS}/exif.min.js ${DOC_STATIC_ASSETS_PATH}/
 \cp -R src/third-party/doc/${DOC_CURRENT_ASSETS_VERSIONS}/images ${DOC_STATIC_ASSETS_PATH}/images
 
 echo "-----------------------------------------------------------------------------------"
-echo "Downloading pdf.js build..."
+echo "Downloading pdf.js files and placing them in third-party directory..."
 echo "-----------------------------------------------------------------------------------"
-curl -L https://github.com/mozilla/pdf.js/releases/download/v${DOC_STATIC_ASSETS_PDFJS_VERSION}/pdfjs-${DOC_STATIC_ASSETS_PDFJS_VERSION}-dist.zip -o pdfjs-dist.zip
-unzip pdfjs-dist.zip -d pdfjs-dist
+# pdf_viewer is not included in the pdf.js build so we get it from a cdn
+curl https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${DOC_STATIC_ASSETS_PDFJS_VERSION}/pdf.min.mjs -o ${DOC_STATIC_ASSETS_PATH}/pdf.min.mjs
+curl https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${DOC_STATIC_ASSETS_PDFJS_VERSION}/pdf.worker.mjs -o ${DOC_STATIC_ASSETS_PATH}/pdf.worker.min.mjs
+curl https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${DOC_STATIC_ASSETS_PDFJS_VERSION}/pdf_viewer.mjs -o ${DOC_STATIC_ASSETS_PATH}/pdf_viewer.mjs
+curl https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${DOC_STATIC_ASSETS_PDFJS_VERSION}/pdf_viewer.min.css -o ${DOC_STATIC_ASSETS_PATH}/pdf_viewer.min.css
 
 echo "-----------------------------------------------------------------------------------"
-echo "Minifying pdf.js files and outputting to thrid-party directory with terser..."
+echo "Minifying pdf_viewer.mjs and outputting to thrid-party directory with terser..."
 echo "-----------------------------------------------------------------------------------"
 TERSER_ARGS=(--comments false -c sequences=false -m reserved=['__webpack_exports__'] --keep-classnames --keep-fnames --module)
-yarn run terser pdfjs-dist/build/pdf.mjs -o ${DOC_STATIC_ASSETS_PATH}/pdf.min.mjs ${TERSER_ARGS[@]}
-yarn run terser pdfjs-dist/build/pdf.worker.mjs -o ${DOC_STATIC_ASSETS_PATH}/pdf.worker.min.mjs ${TERSER_ARGS[@]}
-yarn run terser pdfjs-dist/web/viewer.mjs -o ${DOC_STATIC_ASSETS_PATH}/pdf_viewer.min.mjs ${TERSER_ARGS[@]}
+yarn run terser ${DOC_STATIC_ASSETS_PATH}/pdf_viewer.mjs -o ${DOC_STATIC_ASSETS_PATH}/pdf_viewer.min.mjs --comments false -c sequences=false -m reserved=['__webpack_exports__'] --keep-classnames --keep-fnames --module
+rm ${DOC_STATIC_ASSETS_PATH}/pdf_viewer.mjs
 
 echo "-----------------------------------------------------------------------------------"
-echo "Minifying pdf.js CSS and outputting to thrid-party directory with cssnano..."
+echo "Downloading and copying cmaps to third-party directory..."
 echo "-----------------------------------------------------------------------------------"
-./node_modules/.bin/cssnano pdfjs-dist/web/viewer.css ${DOC_STATIC_ASSETS_PATH}/pdf_viewer.min.css
-
-echo "-----------------------------------------------------------------------------------"
-echo "Copying cmaps to third-party directory..."
-echo "-----------------------------------------------------------------------------------"
+curl -L https://github.com/mozilla/pdf.js/releases/download/v${DOC_STATIC_ASSETS_PDFJS_VERSION}/pdfjs-${DOC_STATIC_ASSETS_PDFJS_VERSION}-dist.zip -o pdfjs-dist.zip
+unzip -q -d pdfjs-dist pdfjs-dist.zip
 \cp -rf pdfjs-dist/web/cmaps ${DOC_STATIC_ASSETS_PATH}/cmaps
-
-echo "-----------------------------------------------------------------------------------"
-echo "Cleanup..."
-echo "-----------------------------------------------------------------------------------"
 rm -rf pdfjs-dist
 rm pdfjs-dist.zip
 
