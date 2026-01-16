02e5473fa86f29d1fae61d36fb0743e07e528304
Sanil Salvi <sanil.salvi@gmail.com>
2026-01-15 21:26:22 -0800

    feat(viedo-preview): revert changes for local development

--- src/lib/viewers/doc/docAssets.js
+++ src/lib/viewers/doc/docAssets.js
@@ -3,10 +3,10 @@ import { DOC_STATIC_ASSETS_VERSION } from '../../constants';
 const STATIC_URI = `third-party/doc/${DOC_STATIC_ASSETS_VERSION}`;
 export const CMAP = `${STATIC_URI}/cmaps/`;
 export const IMAGES = `${STATIC_URI}/images/`;
-export const WORKER = `${STATIC_URI}/pdf.worker.min.js`;
+export const WORKER = `${STATIC_URI}/pdf.worker.min.mjs`;
 
-export const JS = [`${STATIC_URI}/pdf.min.js`, `${STATIC_URI}/pdf_viewer.min.js`, `${STATIC_URI}/exif.min.js`];
-export const JS_NO_EXIF = [`${STATIC_URI}/pdf.min.js`, `${STATIC_URI}/pdf_viewer.min.js`];
+export const JS = [`${STATIC_URI}/pdf.min.mjs`, `${STATIC_URI}/pdf_viewer.min.mjs`, `${STATIC_URI}/exif.min.js`];
+export const JS_NO_EXIF = [`${STATIC_URI}/pdf.min.mjs`, `${STATIC_URI}/pdf_viewer.min.mjs`];
 export const EXIF_READER = [`${STATIC_URI}/exif-reader.min.js`];
 export const CSS = [`${STATIC_URI}/pdf_viewer.min.css`];
 export const PRELOAD_JS = [WORKER];
