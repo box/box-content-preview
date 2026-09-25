a9ba97e3ca5c5979d6f9051e43ae064225ab11b3
Abhishek Marwaha <abhimar1128@gmail.com>
2026-09-25 16:18:17 -0700

    test(audio): name the Cypress playback window previewWindow

--- test/integration/media/MP3Viewer.e2e.test.js
+++ test/integration/media/MP3Viewer.e2e.test.js
@@ -33,18 +33,18 @@ describe('MP3 Viewer', () => {
     const startV2Playback = () => {
         // play() rejects if a load interrupts it. Wait until the file can play, then
         // until playback has started, before a later reload destroys the element.
-        cy.window().should(win => {
-            const audio = win.preview.getCurrentViewer().mediaEl;
+        cy.window().should(previewWindow => {
+            const audio = previewWindow.preview.getCurrentViewer().mediaEl;
             expect(audio, 'audio element').to.exist;
-            expect(audio.readyState).to.be.at.least(win.HTMLMediaElement.HAVE_FUTURE_DATA);
+            expect(audio.readyState).to.be.at.least(previewWindow.HTMLMediaElement.HAVE_FUTURE_DATA);
         });
 
         cy.getByTestId('bp-MP3ControlsV2-play-overlay').click();
         cy.getByTestId('bp-MP3ControlsV2-play-overlay').should('not.exist');
         cy.getByTestId('bp-MP3ControlsV2-bar').should('be.visible');
 
-        cy.window().should(win => {
-            expect(win.preview.getCurrentViewer().mediaEl.paused).to.equal(false);
+        cy.window().should(previewWindow => {
+            expect(previewWindow.preview.getCurrentViewer().mediaEl.paused).to.equal(false);
         });
     };
 
